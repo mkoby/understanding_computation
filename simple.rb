6eadc208d417993bcbc456c334b983f824d49bfe
Michael Koby <michael@mkoby.com>
2015-05-14 19:37:50 -0500

    Initial commit

---
+++ simple.rb
@@ -0,0 +1,126 @@
+class Number < Struct.new( :value ) 
+
+  def reducible?
+    false
+  end
+
+  def to_s
+    value.to_s
+  end
+
+  def inspect
+    "#{self}"
+  end
+
+end
+
+class Add < Struct.new( :left, :right) 
+
+  def reducible?
+    true
+  end
+
+  def reduce
+    if left.reducible?
+      Add.new( left.reduce, right)
+    elsif right.reducible?
+      Add.new( left, right.reduce )
+    else
+      Number.new( left.value + right.value )
+    end
+  end
+
+  def to_s
+    "#{left} + #{right}"
+  end
+
+  def inspect
+    "#{self}"
+  end
+
+end
+
+class Multiply < Struct.new( :left, :right ) 
+
+  def reducible?
+    true
+  end
+
+  def reduce
+    if left.reducible?
+      Multiply.new( left.reduce, right)
+    elsif right.reducible?
+      Multiply.new( left, right.reduce )
+    else
+      Number.new( left.value * right.value )
+    end
+  end
+
+  def to_s
+    "#{left} * #{right}"
+  end
+
+  def inspect
+    "#{self}"
+  end
+
+end
+
+class Boolean < Struct.new( :value )
+
+  def reducible?
+    false
+  end
+
+  def to_s
+    value.to_s
+  end
+
+  def inspect
+    "#{self}"
+  end
+
+end
+
+class LessThan < Struct.new( :left, :right )
+
+  def reducible?
+    true
+  end
+
+  def reduce
+    if left.reducible?
+      LessThan.new( left.reduce, right)
+    elsif right.reducible?
+      LessThan.new( left, right.reduce )
+    else
+      Boolean.new( left.value < right.value )
+    end
+  end
+
+  def to_s
+    "#{left} < #{right}"
+  end
+
+  def inspect
+    "#{self}"
+  end
+
+end
+
+class Machine < Struct.new( :expression )
+
+  def step
+    self.expression = expression.reduce
+  end
+
+  def run
+    while expression.reducible?
+      puts expression
+      step
+    end
+
+    puts expression
+  end
+
+end
