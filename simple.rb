5d5665442fec604b9c4ad519b2b896d6e56a0f5e
Michael Koby <michael@mkoby.com>
2015-06-06 13:03:19 -0500

    Add Variable class

--- simple.rb
+++ simple.rb
@@ -20,11 +20,11 @@ class Add < Struct.new( :left, :right)
     true
   end
 
-  def reduce
+  def reduce( environment )
     if left.reducible?
-      Add.new( left.reduce, right)
+      Add.new( left.reduce(environment), right)
     elsif right.reducible?
-      Add.new( left, right.reduce )
+      Add.new( left, right.reduce(environment) )
     else
       Number.new( left.value + right.value )
     end
@@ -46,11 +46,11 @@ class Multiply < Struct.new( :left, :right )
     true
   end
 
-  def reduce
+  def reduce(environment)
     if left.reducible?
-      Multiply.new( left.reduce, right)
+      Multiply.new( left.reduce(environment), right)
     elsif right.reducible?
-      Multiply.new( left, right.reduce )
+      Multiply.new( left, right.reduce(environment) )
     else
       Number.new( left.value * right.value )
     end
@@ -88,11 +88,11 @@ class LessThan < Struct.new( :left, :right )
     true
   end
 
-  def reduce
+  def reduce(environment)
     if left.reducible?
-      LessThan.new( left.reduce, right)
+      LessThan.new( left.reduce(environment), right)
     elsif right.reducible?
-      LessThan.new( left, right.reduce )
+      LessThan.new( left, right.reduce(environment) )
     else
       Boolean.new( left.value < right.value )
     end
@@ -108,10 +108,30 @@ class LessThan < Struct.new( :left, :right )
 
 end
 
-class Machine < Struct.new( :expression )
+class Variable < Struct.new( :name )
+  
+  def to_s
+    name.to_s
+  end
+
+  def inspect
+    "#{self}"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce( environment )
+    environment[name]
+  end
+
+end
+
+class Machine < Struct.new( :expression, :environment )
 
   def step
-    self.expression = expression.reduce
+    self.expression = expression.reduce(environment)
   end
 
   def run
@@ -124,3 +144,4 @@ class Machine < Struct.new( :expression )
   end
 
 end
+
